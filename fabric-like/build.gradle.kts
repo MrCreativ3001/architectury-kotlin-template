7e296112c2b838a97ba6c55d108909e44b4c50fa
MrCreativ3001 <92984089+MrCreativ3001@users.noreply.github.com>
2023-03-25 23:49:04 +0100

    Fabric-Like now also uses gradle.kts

---
+++ fabric-like/build.gradle.kts
@@ -0,0 +1,19 @@
+
+architectury {
+    common(rootProject.property("enabled_platforms").toString().split(","))
+}
+
+loom {
+    accessWidenerPath.set(project(":common").loom.accessWidenerPath)
+}
+
+dependencies {
+    modImplementation("net.fabricmc:fabric-loader:${rootProject.property("fabric_loader_version")}")
+    modApi("net.fabricmc.fabric-api:fabric-api:${rootProject.property("fabric_api_version")}")
+    // Remove the next line if you don't want to depend on the API
+    modApi("dev.architectury:architectury-fabric:${rootProject.property("architectury_version")}")
+
+    compileClasspath(project(":common", "namedElements")) {
+        isTransitive = false
+    }
+}
