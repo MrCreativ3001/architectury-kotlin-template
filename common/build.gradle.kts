3dc1394f5aa26fb5a591a3132f0bbedbb0d2541d
MrCreativ3001 <92984089+MrCreativ3001@users.noreply.github.com>
2023-03-26 00:44:22 +0100

    Common now uses gradle dsl kotlin

---
+++ common/build.gradle.kts
@@ -0,0 +1,15 @@
+architectury {
+    common(rootProject.property("enabled_platforms").toString().split(","))
+}
+
+loom {
+    accessWidenerPath.set(file("src/main/resources/examplemod.accesswidener"))
+}
+
+dependencies {
+    // We depend on fabric loader here to use the fabric @Environment annotations and get the mixin dependencies
+    // Do NOT use other classes from fabric loader
+    modImplementation("net.fabricmc:fabric-loader:${rootProject.property("fabric_loader_version")}")
+    // Remove the next line if you don't want to depend on the API
+    modApi("dev.architectury:architectury:${rootProject.property("architectury_version")}")
+}
