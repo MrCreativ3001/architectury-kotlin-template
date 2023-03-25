1fe750010e7671390868e539086194b4ade87d32
MrCreativ3001 <92984089+MrCreativ3001@users.noreply.github.com>
2023-03-25 23:45:00 +0100

    Fabric now uses build.gradle.kts

---
+++ fabric/build.gradle.kts
@@ -0,0 +1,93 @@
+plugins {
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+repositories {
+    maven {
+        url = uri("https://maven.quiltmc.org/repository/release/")
+    }
+}
+
+architectury {
+    platformSetupLoomIde()
+    fabric()
+}
+
+loom {
+    accessWidenerPath.set(project(":common").loom.accessWidenerPath)
+}
+
+val common: Configuration by configurations.creating
+val shadowCommon: Configuration by configurations.creating
+val developmentFabric: Configuration by configurations.getting
+
+configurations {
+    compileClasspath.extendsFrom(common)
+    runtimeClasspath.extendsFrom(common)
+    developmentFabric.extendsFrom(common)
+}
+
+dependencies {
+    modImplementation("net.fabricmc:fabric-loader:${rootProject.property("fabric_loader_version")}")
+    modApi("net.fabricmc.fabric-api:fabric-api:${rootProject.property("fabric_api_version")}")
+    // Remove the next line if you don't want to depend on the API
+    modApi("dev.architectury:architectury-fabric:${rootProject.property("architectury_version")}")
+
+    common(project(":common", "namedElements")) {
+        isTransitive = false
+    }
+    shadowCommon(project(":common", "transformProductionFabric")){
+        isTransitive = false
+    }
+    common(project(":fabric-like", "namedElements")){
+        isTransitive = false
+    }
+    shadowCommon(project(":fabric-like", "transformProductionFabric")) {
+        isTransitive = false
+    }
+}
+
+tasks.processResources {
+    inputs.property("group", rootProject.property("maven_group"))
+    inputs.property("version", project.version)
+
+    filesMatching("fabric.mod.json") {
+        expand(mutableMapOf(
+            Pair("group", rootProject.property("maven_group")),
+            Pair("version", project.version),
+            // TODO: Use those in the quilt mod json
+            Pair("minecraft_version", rootProject.property("minecraft_version")),
+            Pair("architectury_version", rootProject.property("architectury_version"))
+        ))
+    }
+}
+
+tasks.shadowJar {
+    exclude("architectury.common.json")
+    configurations = listOf(shadowCommon)
+    classifier = "dev-shadow"
+}
+
+tasks.remapJar {
+    injectAccessWidener.set(true)
+    input.set(tasks.shadowJar.get().archiveFile)
+    dependsOn(tasks.shadowJar)
+    classifier = null
+}
+
+tasks.jar {
+    classifier = "dev"
+}
+
+tasks.sourcesJar {
+    val commonSources = project(":common").tasks.getByName<Jar>("sourcesJar")
+    dependsOn(commonSources)
+    from(commonSources.archiveFile.map { zipTree(it) })
+}
+
+components.getByName("java") {
+    this as AdhocComponentWithVariants
+    this.withVariantsFromConfiguration(project.configurations["shadowRuntimeElements"]) {
+        skip()
+    }
+}
