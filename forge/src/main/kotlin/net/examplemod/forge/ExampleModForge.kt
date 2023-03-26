0f91f4a361fb9168673e19be79a1d88b6df92fc2
MrCreativ3001 <92984089+MrCreativ3001@users.noreply.github.com>
2023-03-26 11:35:10 +0200

    Update ExampleModForge.kt

--- forge/src/main/kotlin/net/examplemod/forge/ExampleModForge.kt
+++ forge/src/main/kotlin/net/examplemod/forge/ExampleModForge.kt
@@ -3,13 +3,14 @@ package net.examplemod.forge
 import dev.architectury.platform.forge.EventBuses
 import net.examplemod.ExampleMod
 import net.minecraftforge.fml.common.Mod
+import thedarkcolour.kotlinforforge.forge.MOD_BUS
 import thedarkcolour.kotlinforforge.forge.MOD_CONTEXT
 
 @Mod(ExampleMod.MOD_ID)
 object ExampleModForge {
     init {
         // Submit our event bus to let architectury register our content on the right time
-        EventBuses.registerModEventBus(ExampleMod.MOD_ID, MOD_CONTEXT.getKEventBus())
+        EventBuses.registerModEventBus(ExampleMod.MOD_ID, MOD_BUS)
         ExampleMod.init()
     }
 }
